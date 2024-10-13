0ccfc244a9d3f3ccf02b2c8f4fa841a8dbfc0125
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-10-13 23:00:28 +0530

    Failed attempt at flash

--- cookbook/prog1/main.c
+++ cookbook/prog1/main.c
@@ -1,26 +1,14 @@
 #include "stm32f4xx.h"
 
-int main(void) {
+int main(void){
 
-  /* The GPIOA clock is initialized by writing 1 to bit-0 of AHB1ENR */
-  RCC->AHB1ENR |= (1U << 0);
+  RCC->AHB1ENR |= (1U << 2);
 
-  /* PA5 is set to output mode by writing 1 to bit-10 of MODER */
-  GPIOA->MODER |= (1U << 10);
-  while (1) {
-    /* Turn ON PA5 by writing 1 to bit-5 of ODR */
-    GPIOA->ODR |= (1U << 5);
-
-    /* Cause delay */
-    for (int i = 0; i < 1000000; i++) {
-    }
-
-    /* Turn OFF PA5 by writing 0 to bit-5 of ODR */
-    GPIOA->ODR &= ~(1U << 5);
-
-    /* Cause delay */
-    for (int i = 0; i < 1000000; i++) {
-    }
+  GPIOC->MODER |= (1U << 26);
+  while(1){
+    GPIOC->ODR |= (1U << 13);
+    for(int i = 0; i < 180000; i++){}
+    GPIOC->ODR &= ~(1U << 13);
+    for(int i = 0; i < 180000; i++){}
   }
-  return 0;
 }
